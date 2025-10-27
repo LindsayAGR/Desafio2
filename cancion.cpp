d36c976057541aeb87a94095ff71c05851ae13c9
Lindsay <lindsay.garcia@udea.edu.co>
2025-10-26 20:43:04 -0500

    Implemento la clase canción con constructores y destructores

---
+++ cancion.cpp
@@ -0,0 +1,58 @@
+#include "cancion.h"
+
+cancion::cancion() {
+    id = 0;
+    nomCan = new char[50]; strcpy(nomCan, "");
+    artista = new char[50]; strcpy(artista, "");
+    album = new char[50]; strcpy(album, "");
+    genero = new char[30]; strcpy(genero, "");
+    duracion = 0;
+}
+cancion::cancion(int id, const char *nomCan, const char *artista, const char *album, const char *genero, int duracion){
+    id = id;
+    
+    nomCan = new char[strlen(nomCan)+1];
+    strcpy(nomCan,nomCan);
+    
+    artista = new char[strlen(artista)+1];
+    strcpy(artista,artista);
+        
+    album = new char[strlen(album) + 1];
+    strcpy(album, album);
+
+    genero = new char[strlen(genero) + 1];
+    strcpy(genero, genero);
+    
+    duracion = duracion;
+}
+//constructor copia
+cancion::cancion(const cancion& otra) {
+    id = otra.id;
+    nomCan = new char[strlen(otra.nomCan) + 1];
+    strcpy(nomCan, otra.nomCan);
+    
+    artista = new char[strlen(otra.artista) + 1];
+    strcpy(artista, otra.artista);
+    
+    album = new char[strlen(otra.album) + 1];
+    strcpy(album, otra.album);
+    
+    genero = new char[strlen(otra.genero) + 1];
+    strcpy(genero, otra.genero);
+    
+    duracion = otra.duracion;
+}
+//destructor
+cancion::~cancion() {
+    delete[] nomCan;
+    delete[] artista;
+    delete[] album;
+    delete[] genero;
+}
+//getter
+int cancion::getId() const { return id; }
+const char* cancion::getNombre() const { return nomCan; }
+const char* cancion::getArtista() const { return artista; }
+const char* cancion::getAlbum() const { return album; }
+const char* cancion::getGenero() const { return genero; }
+int cancion::getDuracion() const { return duracion; }
