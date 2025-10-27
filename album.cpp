a2635a48964e9d5b7c254d7987ff5da6c337b9c9
Lindsay <lindsay.garcia@udea.edu.co>
2025-10-26 22:01:34 -0500

    Add getters and file loading for album class

--- album.cpp
+++ album.cpp
@@ -64,3 +64,55 @@ album::~album() {
     delete[] rutaPortada;
     delete[] duracionTotal;
 }
+//getter
+int album::getId() const { return id; }
+const char* album::getNombre() const { return nombre; }
+const char* album::getArtista() const { return artista; }
+int album::getAnio() const { return anio; }
+const char* album::getGenero() const { return genero; }
+const char* album::getRutaPortada() const { return rutaPortada; }
+const char* album::getDuracionTotal() const { return duracionTotal; }
+
+
+// Cargar datos desde archivo
+
+void album::cargarDesdeArchivo(ifstream& archivo) {
+    char linea[300];
+
+    if (archivo.getline(linea, 300)) {
+        char* token = strtok(linea, "|");
+        if (!token) return;
+        id = atoi(token);
+
+        token = strtok(NULL, "|");
+        strcpy(nombre, token);
+
+        token = strtok(NULL, "|");
+        strcpy(artista, token);
+
+        token = strtok(NULL, "|");
+        anio = atoi(token);
+
+        token = strtok(NULL, "|");
+        strcpy(genero, token);
+
+        token = strtok(NULL, "|");
+        strcpy(rutaPortada, token);
+
+        token = strtok(NULL, ";");
+        strcpy(duracionTotal, token);
+    }
+}
+
+// Mostrar información del álbum
+void album::mostrarInfo() const {
+    cout << "\n--------------------------------------\n";
+    cout << "ID: " << id << endl;
+    cout << "Nombre: " << nombre << endl;
+    cout << "Artista: " << artista << endl;
+    cout << "Año: " << anio << endl;
+    cout << "Género: " << genero << endl;
+    cout << "Ruta de portada: " << rutaPortada << endl;
+    cout << "Duración total: " << duracionTotal << endl;
+}
+
